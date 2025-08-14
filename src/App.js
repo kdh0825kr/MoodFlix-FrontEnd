608374c9607f7b41ec19a43107c425bb1582dcec
kdh <b_kdh@naver.com>
2025-08-15 00:18:58 +0900

    feat: 홈 화면 구현 및 다크 테마 적용
    - 홈 화면 컴포넌트 생성 (HomeScreen.js, HomeScreen.css)
    - useMood 훅에 홈 화면 상태 관리 추가
    - App.js에서 홈 화면과 메인 페이지 간 전환 구현
    - 사이드바 홈 버튼 기능 제거
    - 홈 화면 다크 테마 색상 적용
    - 반응형 디자인 및 애니메이션 효과 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
+import HomeScreen from './components/HomeScreen';
 import { useMood } from './hooks/useMood';
 
 function App() {
@@ -9,22 +10,32 @@ function App() {
     selectedMood,
     moodDescription,
     canRecommend,
+    showHome,
     handleMoodSelect,
     handleMoodDescriptionChange,
-    handleGetRecommendations
+    handleGetRecommendations,
+    handleStartApp,
+    handleGoHome
   } = useMood();
 
   return (
-    <div className="app">
-      <Sidebar />
-      <MainContent 
-        selectedMood={selectedMood}
-        moodDescription={moodDescription}
-        canRecommend={canRecommend}
-        onMoodSelect={handleMoodSelect}
-        onMoodDescriptionChange={handleMoodDescriptionChange}
-        onGetRecommendations={handleGetRecommendations}
-      />
+    <div className={`app ${!showHome ? 'home-view' : ''}`}>
+      {showHome ? (
+        <HomeScreen onStart={handleStartApp} />
+      ) : (
+        <>
+          <Sidebar />
+          <MainContent 
+            selectedMood={selectedMood}
+            moodDescription={moodDescription}
+            canRecommend={canRecommend}
+            onMoodSelect={handleMoodSelect}
+            onMoodDescriptionChange={handleMoodDescriptionChange}
+            onGetRecommendations={handleGetRecommendations}
+            onGoHome={handleGoHome}
+          />
+        </>
+      )}
     </div>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import EmotionSelector from './EmotionSelector';
 import MoodInput from './MoodInput';
 import RecommendButton from './RecommendButton';
+import { FaHome } from 'react-icons/fa';
 import './MainContent.css';
 
 const MainContent = ({ 
@@ -11,14 +12,24 @@ const MainContent = ({
   canRecommend,
   onMoodSelect, 
   onMoodDescriptionChange, 
-  onGetRecommendations 
+  onGetRecommendations,
+  onGoHome
 }) => {
   return (
     <main className="main-content" aria-labelledby="main-title">
       <div className="content-card">
         {/* 메인 타이틀 섹션 */}
         <div className="title-section">
-          <h1 id="main-title" className="main-title">당신의 감정을 영화로</h1>
+          <div className="title-header">
+            <h1 id="main-title" className="main-title">당신의 감정을 영화로</h1>
+            <button 
+              className="home-button"
+              onClick={onGoHome}
+              aria-label="홈으로 돌아가기"
+            >
+              <FaHome />
+            </button>
+          </div>
           <p className="subtitle">오늘의 기분에 맞는 완벽한 영화를 찾아보세요</p>
         </div>
 
@@ -58,6 +69,7 @@ MainContent.propTypes = {
   onMoodSelect: PropTypes.func.isRequired,
   onMoodDescriptionChange: PropTypes.func.isRequired,
   onGetRecommendations: PropTypes.func.isRequired,
+  onGoHome: PropTypes.func.isRequired,
 };
 
 MainContent.defaultProps = {
